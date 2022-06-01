58fe1955c7f9ce817f93d7e32aaefcd608762f72
Pawel <pawel.jest@interia.pl>
2022-06-01 18:21:11 +0200

    Dostosowanie projektu: - 6.2. Czym jest Test Driven Development (TDD)?

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+
+        int result1 = calculator.substractAFromB(9,6);
+
+        if (result1 == 3) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int result2 = calculator.addAToB(9,6);
+
+        if (result2 == 15) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
